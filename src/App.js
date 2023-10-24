69015cdf7696e204a5379ba2d489098328ea74fb
Winnie <winniebethwanjiru9@gmail.com>
2023-10-25 00:31:21 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './App.css';
+import Data from './components/Data';
+import Categories from './components/Categories';
+import Menu from './components/Menu';
+
+const allCategories = ['all', ...new Set(Data.map(item => item.category))]
+
+
+function App() {
+  const [menu, setMenuItems] = useState(Data);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(Data);
+      return;
+    }
+    const newItems = Data.filter((item) => item.category === category);
+    setMenuItems(newItems)
+  };
+
+
+  return (
+    <main>
+      <section className="main-container">
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />       
+         <Menu menu={menu} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
+
+//you are passing the Data variable as a prop to the MenuItem component and naming it menu. 
+//This indicates that you intend to provide menu data to the MenuItem component, 
+//and the name menu is a clear and concise choice for the prop.
+//Using Data as the prop name
+//menu is the data you're passing to the MenuItem component.
---
+++ src/components/Data.js
@@ -0,0 +1,60 @@
+const menu = [
+    {
+        id: 1,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: "$15.80",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt quo obcaecati corrupti dolorem deserunt fuga beatae assumenda voluptates ullam",
+        img: "https://plus.unsplash.com/premium_photo-1672846027109-e2c91500afef?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFuY2FrZXN8ZW58MHx8MHx8fDA%3D",
+      },
+    {
+        id: 2,
+        title: "diner double",
+        category: "lunch",
+        price: "$13.99",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt quo obcaecati corrupti dolorem deserunt fuga beatae assumenda voluptates ullam",
+        img: "https://plus.unsplash.com/premium_photo-1672846027109-e2c91500afef?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFuY2FrZXN8ZW58MHx8MHx8fDA%3D",
+      },
+    {
+        id: 3,
+        title: "godzilla milkshake",
+        category: "shakes",
+        price: "$6.99",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt quo obcaecati corrupti dolorem deserunt fuga beatae assumenda voluptates ullam",
+        img: "https://plus.unsplash.com/premium_photo-1672846027109-e2c91500afef?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFuY2FrZXN8ZW58MHx8MHx8fDA%3D",
+      },
+    {
+        id: 4,
+        title: "country delight",
+        category: "breakfast",
+        price: "$20.99",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt quo obcaecati corrupti dolorem deserunt fuga beatae assumenda voluptates ullam",
+        img: "https://plus.unsplash.com/premium_photo-1672846027109-e2c91500afef?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFuY2FrZXN8ZW58MHx8MHx8fDA%3D",
+      },
+    {
+        id: 5,
+        title: "egg attack",
+        category: "lunch",
+        price: "$22.99",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt quo obcaecati corrupti dolorem deserunt fuga beatae assumenda voluptates ullam",
+        img: "https://plus.unsplash.com/premium_photo-1672846027109-e2c91500afef?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFuY2FrZXN8ZW58MHx8MHx8fDA%3D",
+      },
+    {
+      id: 6,
+      title: "coffee",
+      category: "breakfast",
+      price: "$1.80",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt quo obcaecati corrupti dolorem deserunt fuga beatae assumenda voluptates ullam",
+      img: "https://plus.unsplash.com/premium_photo-1672846027109-e2c91500afef?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFuY2FrZXN8ZW58MHx8MHx8fDA%3D",
+    },
+  {
+    id: 7,
+    title: "steak",
+    category: "dinner",
+    price: "$39.99",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt quo obcaecati corrupti dolorem deserunt fuga beatae assumenda voluptates ullam",
+    img: "https://plus.unsplash.com/premium_photo-1672846027109-e2c91500afef?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFuY2FrZXN8ZW58MHx8MHx8fDA%3D",
+  }
+
+];
+export default menu;
